fbf7a70201cd01812e893f6651db0421979583f6
ARIFSERYUL <116109007+ARIFSERYUL@users.noreply.github.com>
2023-09-22 01:04:23 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const items = [
+    { id: Math.random(), title: 'وجبة افطار ', description: 'وجبة افطار لشخص واحد: تتكون من قطعتين من خبز التوست وبيضة وعدة قطع من الطماطم  ', price: ' 100 جنيه', category: 'افطار', imgUrl: "1.png" },
+    { id: Math.random(), title: 'وجبه غداء', description: 'وصف الغداء', price: '80 جنيه', category: 'غداء', imgUrl: "3.png" },
+    { id: Math.random(), title: 'وجبه عشاء', description: 'وصف العشاء', price: '40 جنيه', category: 'عشاء', imgUrl: "5.png" },
+    { id: Math.random(), title: 'وجبه غداء ', description: 'وصف الغداء', price: '30 جنيه', category: 'غداء', imgUrl: "4.png" },
+    { id: Math.random(), title: 'وجبه عشاء درجه اولى', description: 'وصف العشاء', price: '50 جنيه', category: 'عشاء', imgUrl: "5.png" },
+    { id: Math.random(), title: 'وجبه افطار', description: 'وصف الإفطار', price: '50 جنيه', category: 'افطار', imgUrl: "2.png" },
+    { id: Math.random(), title: 'وجبه غداء', description: 'وصف الغداء', price: '50 جنيه', category: 'غداء', imgUrl: "4.png" }
+]
